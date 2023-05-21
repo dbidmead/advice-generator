2dcb86a5235f38eae931fa3dd8487629a85810ab
Drew Bidmead <dmbidmead@gmail.com>
2023-05-21 14:20:33 -0400

    Transitions improved

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const textBody = document.querySelector('.container');
+const containerEl = document.querySelector('.container');
+const textBody = document.querySelector('.text-content');
 const idDisplay = document.querySelector('h3');
 const textDisplay = document.querySelector('h1');
 const button = document.querySelector('#btn');
 
 let adviceMessage = '';
 let idMessage = '';
+ console.log(window.innerWidth)
 
 
 async function getAdvice() {
@@ -17,20 +19,46 @@ async function getAdvice() {
     return data;
 }
 
+function handleTransition() {
+    containerEl.removeEventListener('transitionend', handleTransition);
+    containerEl.style.height = null;
+    newHeight = null;
+}
+
 // to access the data object, you need to call the async function FIRST to wait for its return value 
 // THEN perform the callback function which takes the async return as a parameter
 getAdvice().then((data) => console.log(data))
 
 button.addEventListener('click', () => {
-    textBody.setAttribute('style', 'opacity:0')
-    // wait for the getAdvice() function to set the message string values
-    getAdvice().then(() => {
-         // then, set the DOM element textContents based on the message string values
-        idDisplay.textContent = `ADVICE # ${idMessage}`
-        textDisplay.textContent = adviceMessage;
-    });
-    setTimeout(() => {
-        textBody.setAttribute('style', 'opacity: 1')
-    },500)
+    let dividerEl = window.innerWidth < 550 ? 
+        document.querySelector('#mobile-divider') :
+        document.querySelector('#desktop-divider');
 
+    let currentHeight = getComputedStyle(containerEl).height;
+    containerEl.style.height = currentHeight;
+
+    textBody.setAttribute('style', 'opacity:0')
+    dividerEl.setAttribute('style', 'opacity:0')
+
+    setTimeout(() => {
+        containerEl.style.height = 0
+    },200)
+
+    setTimeout(() => {
+        // wait for the getAdvice() function to set the message string values
+        getAdvice().then(() => {
+            // then, set the DOM element textContents based on the message string values
+            idDisplay.textContent = `ADVICE # ${idMessage}`
+            textDisplay.textContent = adviceMessage;
+        });
+        setTimeout(() => {
+            let newHeight = containerEl.scrollHeight;
+            containerEl.addEventListener('transitionend', handleTransition)
+            containerEl.style.height = `${newHeight}px`;
+        },200)
+        setTimeout(() => {
+            textBody.setAttribute('style', 'opacity: 1');
+            dividerEl.setAttribute('style', 'opacity: 1');
+        },600)
+    },600)
 })
